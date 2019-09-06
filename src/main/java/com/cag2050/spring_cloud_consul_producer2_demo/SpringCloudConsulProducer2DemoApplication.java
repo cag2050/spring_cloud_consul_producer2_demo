a6f7f4408030b38611313d01c8c1e992f06de93f
chen_anguo <cag2050@163.com>
2019-09-06 11:46:26 +0800

    project init

---
+++ src/main/java/com/cag2050/spring_cloud_consul_producer2_demo/SpringCloudConsulProducer2DemoApplication.java
@@ -0,0 +1,13 @@
+package com.cag2050.spring_cloud_consul_producer2_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConsulProducer2DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConsulProducer2DemoApplication.class, args);
+    }
+
+}
