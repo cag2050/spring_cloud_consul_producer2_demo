b442b46dfca2ad465333148521eb0d4b06585bca
chen_anguo <cag2050@163.com>
2019-09-06 14:54:27 +0800

    add: @EnableDiscoveryClient

--- src/main/java/com/cag2050/spring_cloud_consul_producer2_demo/SpringCloudConsulProducer2DemoApplication.java
+++ src/main/java/com/cag2050/spring_cloud_consul_producer2_demo/SpringCloudConsulProducer2DemoApplication.java
@@ -2,8 +2,10 @@ package com.cag2050.spring_cloud_consul_producer2_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringCloudConsulProducer2DemoApplication {
 
     public static void main(String[] args) {
